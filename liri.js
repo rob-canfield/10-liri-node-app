ebb6d03828f48021f8860c0fb6fad950f718617d
Rob Canfield <git@livesenjoyable.com>
2019-09-14 21:30:56 -0600

    all search ptions are working in an if statement. if nothing is searched, basic instructons for the app are consoled. still need to get do-what-it-says working and display specific information for each search. top notch, top notch.

--- liri.js
+++ liri.js
@@ -10,60 +10,58 @@ var spotify = new Spotify(keys.spotify);
 var argument1 = process.argv[2]
 var argument2 = process.argv.slice(3).join(" ");
 
-// var nodeArgs = process.argv;
-
-
 if (argument1 === "spotify-this") {
 
   var artist = argument2
 
   spotify.search({ type: 'artist', query: artist }, function(err, data) {
-  
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  
-    console.log(data.artists.items[0]);
-  
-  });
 
+  if (err) {
+  return console.log('Error occurred: ' + err);
+  }
+
+  console.log(data.artists.items[0]);
+
+});
+
+} 
+
+else if (argument1 === "concert-this") {
+
+  var artist = argument2
+  var bandsintown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+  
+  axios.get(bandsintown).then(
+    function(response) {
+      console.log(response);
+    })
+}
+
+else if (argument1 === "movie-this") {
+
+  var movie = argument2
+  var ombd = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+
+  axios.get(ombd).then(
+    function (response) {
+      console.log(response);
+    })
+
+}
+
+else if (argument1 === "do-what-it-says") {
+  
+}
+
+else {
+  console.log('\n†††††††††††††††††††††††††††††††††††††††††††††††††††††\n\nWelcome to Liri v.0.0.0.1. \n\nSearch songs, concerts, and movies using the following commands:\n\n·spotify-this-song\n·concert-this \n·movie-this \n\n-or- \n\n·do-what-it-says for a random search\n')
 }
 
 
 
 
-// var artist = process.argv[3]
-// var bandsintown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-// axios.get(bandsintown).then(
-//   function(response) {
-//     console.log(response);
-//   })
 
 
-// var movie = process.argv[3]
-// var ombd = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
 
-// for (var i = 2; i < nodeArgs.length; i++) {
 
-//     if (i > 2 && i < nodeArgs.length) {
-//       movie = movie + "+" + nodeArgs[i];
-//     } else {
-//       movie += nodeArgs[i];
-  
-//     }
-//   }
-
-// axios.get(ombd).then(
-// function(response) {
-//     console.log(response);
-// })
-
-// var movie = process.argv[3]
-// var ombd = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
-
-// axios.get(ombd).then(
-// function(response) {
-//     console.log(response);
-// })
 
